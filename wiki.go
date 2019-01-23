88531fd20dfa74fa382ec5e24663db3785a922b4
Vishnu Nair <vish@vnair.me>
2019-01-23 14:03:56 -0500

    Adding save and edit handlers.

--- wiki.go
+++ wiki.go
@@ -28,6 +28,8 @@ func loadPage(title string) (*Page, error) {
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -35,8 +37,41 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
 	if err != nil {
-		fmt.Fprintf(w, "<h1>An error occurred while retrieving: %s</h1>", title)
+		fmt.Fprintf(w, "<h1>An error occurred while retrieving: %s</h1><div>%s</div>", title, err.Error())
 	} else {
 		fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	}
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w, "<h1>An error occurred while attempting to save: %s</h1><div>%s</div>", title, err)
+		return
+	}
+	body := r.Form.Get("body")
+	pg := &Page{Title: title, Body: []byte(body)}
+	err = pg.save()
+	if err != nil {
+		fmt.Fprintf(w, "<h1>An error occurred while attempting to save: %s</h1><div>%s</div>", title, err)
+		return
+	} else {
+		fmt.Fprintf(w, "<h1>Post saved!</h1>", title)
 	}
 }
