c7559ac3d8dff4dd91a1bd4bdcb68ac11eee2dd3
Vishnu Nair <vish@vnair.me>
2019-01-23 20:26:18 -0500

    Adding comments.

--- wiki.go
+++ wiki.go
@@ -1,3 +1,4 @@
+// Runs an HTTP server that serves a simple wiki-style app.
 package main
 
 import (
@@ -13,18 +14,22 @@ var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.h
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 var interPageLink = regexp.MustCompile(`\[([a-zA-Z0-9]*)\]`)
 
+// Page represents a single page/article in this wiki.
 type Page struct {
-	Title    string
-	Body     []byte
-	DispBody template.HTML
-	FromSave bool
+	Title    string        // page title
+	Body     []byte        // page body
+	DispBody template.HTML // page body in displayable form (i.e., links expanded out)
+	FromSave bool          // whether or not this page object was created following a save operation
 }
 
+// Page.save() saves a Page's title and body into a simple text file in the data/ folder.
 func (p *Page) save() error {
 	filename := "data/" + p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
+// loadPage takes in a target title and looks for the desired page in the data folder. If successful, a Page object
+// is returned with the target data. If not successful, the Page return value is nil, and an error is instead returned.
 func loadPage(title string) (*Page, error) {
 	filename := "data/" + title + ".txt"
 	body, err := ioutil.ReadFile(filename)
@@ -38,6 +43,7 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body, DispBody: dispBody}, nil
 }
 
+// renderTemplate renders the desired template using Page data and writes the resulting response into a ResponseWriter.
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
@@ -46,6 +52,8 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
+// viewHandler handles view requests. If the Page exists, the view template is rendered. If it does not exist,
+// the handler redirects to the edit endpoint.
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 
@@ -63,6 +71,8 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	renderTemplate(w, "view", p)
 }
 
+// editHandler handles page edit requests. A Page object is used to render the edit template. If the Page does not
+// exist, the Page.Body component will be empty.
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
@@ -71,6 +81,8 @@ func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	renderTemplate(w, "edit", p)
 }
 
+// saveHandler handles requests to save an edited page to the file. Only POST requests are accepted. The user
+// is then redirected to the viewHandler upon a successful save.
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	// handle direct access to URL.
 	if r.Method != http.MethodPost {
@@ -88,6 +100,9 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	http.Redirect(w, r, "/view/"+title+"?from_save=true", http.StatusFound)
 }
 
+// makeHandler converts existing functions that accept (w http.ResponseWriter, r *http.Request, title string) into
+// handlers compatible with http.HandlerFunc, with parameters (w http.ResponseWriter, r *http.Request), by parsing out
+// the name of the desired file/page title from the requests's URL and passing it into these existing functions.
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
@@ -99,10 +114,13 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	}
 }
 
+// redirFrontPage redirects automatically to the FrontPage via a 302 redirect. It is meant to handle requests to the
+// web root ("/") and is compatible with http.HandlerFunc.
 func redirFrontPage(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/FrontPage", http.StatusTemporaryRedirect)
 }
 
+// main registers the handlers and executes the HTTP server.
 func main() {
 	http.HandleFunc("/", redirFrontPage)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
